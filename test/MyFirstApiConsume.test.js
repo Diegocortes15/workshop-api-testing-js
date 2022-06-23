b0b481b65ceb11dd9a8a2fa75af02e976137aaeb
Diego Alejandro <cortesroadiegoalejandro@gmail.com>
2022-06-23 01:09:02 -0500

    Removed axios interceptors. Added new expects in HEAD, PATCH, PUT and DELETE tests cases

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,51 +2,6 @@ const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
-axios.interceptors.request.use((request) => {
-	const headers = {
-		...request.headers.common,
-		...request.headers[request.method],
-		...request.headers,
-	};
-
-	// remove unrelevant headers to reduce the noise
-	['common', 'get', 'post', 'head', 'put', 'patch', 'delete'].forEach(
-		(header) => {
-			delete headers[header];
-		}
-	);
-
-	const printable = `
----------------------------------------------- \n
-REQUEST ⏩ \n
-${new Date()}
-Request: ${request.method.toUpperCase()}
-URL:${request.url}
-Headers: ${JSON.stringify(headers, null, 2)} \n
-Data: ${
-		request.data ? JSON.stringify(response.data, null, 2) : 'There is no data'
-	}
-	`;
-
-	console.log(printable);
-	return request;
-});
-
-axios.interceptors.response.use((response) => {
-	const printable = `
-⏩ RESPONSE \n
-${new Date()}
-Status: ${response.status} ${response.statusText}
-Headers: ${JSON.stringify(response.headers, null, 2)} \n
-Data: ${
-		response.data ? JSON.stringify(response.data, null, 2) : 'There is no data'
-	}
-`;
-
-	console.log(printable);
-	return response;
-});
-
 describe('First Api Tests', () => {
 	it('Consume GET Service', async () => {
 		const response = await axios.get('https://httpbin.org/ip');
@@ -69,24 +24,39 @@ describe('First Api Tests', () => {
 	});
 
 	it('Consume HEAD Service', async () => {
-		const response = await axios.get('https://httpbin.org/headers');
+		const response = await axios.head('https://httpbin.org/headers');
 
 		expect(response.status).to.equal(StatusCodes.OK);
-		expect(response.data).to.have.property('headers');
+		expect(response.config.url).to.have.string('/head');
+		expect(response.data).to.be.empty;
 	});
 
 	it('Consume PATCH Service', async () => {
-		const response = await axios.patch('https://httpbin.org/patch');
+		const query = {
+			name: 'Diego Alejandro',
+			surname: 'Cortes Roa',
+			career: 'Multimedia Engineer',
+		};
+
+		const response = await axios.patch('https://httpbin.org/patch', query);
 
 		expect(response.status).to.equal(StatusCodes.OK);
 		expect(response.config.url).to.have.string('/patch');
+		expect(JSON.parse(response.data.data)).to.eql(query);
 	});
 
 	it('Consume PUT Service', async () => {
-		const response = await axios.put('https://httpbin.org/put');
+		const query = {
+			name: 'Diego Alejandro',
+			surname: 'Cortes Roa',
+			career: 'Multimedia Engineer',
+		};
+
+		const response = await axios.put('https://httpbin.org/put', query);
 
 		expect(response.status).to.equal(StatusCodes.OK);
 		expect(response.config.url).to.have.string('/put');
+		expect(JSON.parse(response.data.data)).to.eql(response.data.json);
 	});
 
 	it('Consume DELETE Service', async () => {
@@ -94,5 +64,6 @@ describe('First Api Tests', () => {
 
 		expect(response.status).to.equal(StatusCodes.OK);
 		expect(response.config.url).to.have.string('/delete');
+		expect(response.data.data).to.be.empty;
 	});
 });
