12a99c97fa8568136e5b9d6657bb32fd64d251c2
Diego Cortés <60171460+Diegocortes15@users.noreply.github.com>
2022-06-28 10:04:27 -0500

    Added tests with github api (#6)
    * Added test with github api
    
    * fixing github actions issue
    
    * fixing github actions issue
    
    * Added UrlBase as variable

---
+++ test/GithubApi.Authentication.test.js
@@ -0,0 +1,39 @@
+const { StatusCodes } = require('http-status-codes');
+const { expect } = require('chai');
+const axios = require('axios');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'Diegocortes15';
+const repository = 'workshop-api-testing-js';
+
+describe('Github Api Test', () => {
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', async () => {
+      const response = await axios.get(
+        `${urlBase}/repos/${githubUserName}/${repository}`,
+        {
+          headers: {
+            Authorization: `${process.env.ACCESS_TOKEN}`
+          }
+        }
+      );
+
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal(
+        'This is a Workshop about Api Testing in JavaScript'
+      );
+    });
+
+    it('Via OAuth2 Tokens by parameter', async () => {
+      const response = await axios.get(
+        `${urlBase}/repos/${githubUserName}/${repository}`,
+        { access_token: process.env.ACCESS_TOKEN }
+      );
+
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal(
+        'This is a Workshop about Api Testing in JavaScript'
+      );
+    });
+  });
+});
