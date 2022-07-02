e30ffb8a68f1476724195f414b4ba3eec9be909e
Diego Alejandro <cortesroadiegoalejandro@gmail.com>
2022-07-01 19:35:43 -0500

    Consuming PUT methods

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -13,7 +13,7 @@ describe('Github Api Test', () => {
         `${urlBase}/repos/${githubUserName}/${repository}`,
         {
           headers: {
-            Authorization: `${process.env.ACCESS_TOKEN}`
+            Authorization: `token ${process.env.ACCESS_TOKEN}`
           }
         }
       );
