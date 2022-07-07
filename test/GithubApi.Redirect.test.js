56cf0989a383cc358f1e117661706ee009b1eb3c
Diego <cortesroadiegoalejandro@gmail.com>
2022-07-07 14:46:22 -0500

    Updating test cases as independets

--- test/GithubApi.Redirect.test.js
+++ test/GithubApi.Redirect.test.js
@@ -2,26 +2,23 @@ const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
-const repository = {
-  url: 'https://github.com/aperdomob/redirect-test'
-};
+const repositoryUrl = 'https://github.com/aperdomob/redirect-test';
+const expectedRedirectUrl = 'https://github.com/aperdomob/new-redirect-test';
 
 describe('Consume HEAD method and test redirecting url', () => {
   it('Verify that response has redirected url with HEAD method', async () => {
-    const response = await axios.head(repository.url);
+    const response = await axios.head(repositoryUrl);
 
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.request.res.responseUrl).to.equal(
       'https://github.com/aperdomob/new-redirect-test'
     );
-
-    repository.redirectedUrl = response.request.res.responseUrl;
   });
 
   it('Verify that response has redirected url with GET method', async () => {
-    const response = await axios.get(repository.url);
+    const response = await axios.get(repositoryUrl);
 
     expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.request.res.responseUrl).to.equal(repository.redirectedUrl);
+    expect(response.request.res.responseUrl).to.equal(expectedRedirectUrl);
   });
 });
